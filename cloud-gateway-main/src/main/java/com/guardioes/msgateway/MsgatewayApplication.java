322ddb1155ed208ee7d26a3393c4466a1d9aded9
Joao Pedro <joaopedrogolenia@gmail.com>
2024-08-12 16:08:32 -0300

    Fix:Resolvendo problema do eureka

---
+++ cloud-gateway-main/src/main/java/com/guardioes/msgateway/MsgatewayApplication.java
@@ -0,0 +1,29 @@
+package com.guardioes.msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/api/v1/pasteis/**").uri("lb://cadastramento"))
+//					.route(r -> r.path("/api/v1/usuarios/**").uri("lb://cadastrousuarios"))
+//					.route(r -> r.path("/item/**").uri("lb://pedidos"))
+//				    .route(r -> r.path("/pedidos/**").uri("lb://pedidos"))
+				.build();
+	}
+
+}
